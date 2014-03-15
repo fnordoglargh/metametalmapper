c3bc5887f49914de00066c646417e81320c4a920
Martin Woelke <fnordoglargh@gmail.com>
2014-03-15 23:54:22 +0100

    Rest of script...

--- metalMapper.py
+++ metalMapper.py
@@ -21,17 +21,56 @@ while (searchDepth > 0):
     indexBand = website.find('">',indexBand+1)
     indexBand = website.find('">',indexBand+1)
     indexBandEnd = website.find('</',indexBand)
+    
+    bandName = website[indexBand+2:indexBandEnd]
+    print 'Band: ' + bandName
 
-print 'Band: ' + website[indexBand+2:indexBandEnd]
-
-isEndReached = False
-
-while (not isEndReached):
+    isEndReached = False
+    loopBandLink = 'Empty'
+    loopBandName = 'Empty'
     seeAlsoIndex = website.find('See also:')
-    print seeAlsoIndex
+    tableElementEndIndex = 0
+    loopbands = 'Empty'
 
-    if (seeAlsoIndex != -1):
-        equalIndex = website.find('=')
-        print equalIndex
-    else:
-        isEndReached = True
+    while (not isEndReached):
+    
+        if (seeAlsoIndex != -1):
+            equalIndex = website.find('="',seeAlsoIndex)
+            tableElementEndIndex = website.find('</td>',equalIndex)
+            loopBands = website[equalIndex:tableElementIndex]
+            bandsListRaw = loopBands.split(',')
+
+            for s in BandsListRaw:
+                linkIndex = s.find('http:')
+                # Look here for "ex"-Bands
+
+                if linkIndex != -1:
+                    linkEndIndex = s.find('"', linkIndex)
+                    loopBandLink = s[linkIndex:linkEndIndex]
+                    bandsToVisit.append(loopBandLink)
+                    loopBandName = s[s.find('">')+2:s.find('</a>')]
+                else:
+                    loopBandName = s.lstrip().rstrip()
+                    
+                    if(loopBandName.find('ex') != -1):
+                        loopBandName[loopBandName.find('ex')+3:]
+                        # get rid of trailing "live"
+                bandsList.append('\t"' + bandName + '" -- "' + loopBandName+ '":')
+        else:
+            isEndReached = True
+
+        seeAlsoIndex = website.find('See also:', seeAlsoIndex + 1)
+
+    searchDepth-=1
+
+bandsList.append('}')
+bandsFile = open('bandsGraph.dot', 'w')
+
+for s in bandsList:
+    bandsFile.write("%s\n" % s)
+
+print 'Visited [' + str(len(bandsVisited)) + '] bands.'
+
+bandsFile.close()
+os.System("fdp -Tpng bandsGraph.dot -o bandsGraph.png")
+    
