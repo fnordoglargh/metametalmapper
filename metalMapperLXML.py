bfb99dd69eaddced751235e8bda4b3027027b7e4
Martin Woelke <fnordoglargh@gmail.com>
2014-06-28 21:46:25 +0200

    Trying to get the band name.

--- metalMapperLXML.py
+++ metalMapperLXML.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
 import urllib2,os
-from lxml import etree
+import lxml.html
 
 bandsToVisit = ['http://www.metal-archives.com/bands/Entombed/7']
 bandsVisited = list()
+
+searchDepth = 1
 
 while (searchDepth > 0):
 
@@ -12,7 +14,11 @@ while (searchDepth > 0):
     bandsVisited.append(bandCurrentlyVisiting)
 
     website = urllib2.urlopen(bandCurrentlyVisiting).read()
+    html = lxml.html.fromstring(website)#.getroot()
+    print(html.xpath("//@class='band_name'"))
 
+
+#    print lxml.html.tostring(html)
     searchDepth-=1
 
 print 'Visited [' + str(len(bandsVisited)) + '] bands.'
