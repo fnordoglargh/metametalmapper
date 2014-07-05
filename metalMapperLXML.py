3a0df59b511063ed81d32b23fd16c33dfc66e8ee
Martin Woelke <fnordoglargh@gmail.com>
2014-07-05 22:36:36 +0200

    Indentation changed.

--- metalMapperLXML.py
+++ metalMapperLXML.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 def has_class(tag):
-    return tag.has_attr('class')
+	return tag.has_attr('class')
 
 bandsToVisit = ['http://www.metal-archives.com/bands/Entombed/7']
 bandsVisited = list()
@@ -16,20 +16,29 @@ searchDepth = 1
 
 while (searchDepth > 0):
 
-    bandCurrentlyVisiting = bandsToVisit.pop()
-    bandsVisited.append(bandCurrentlyVisiting)
+	bandCurrentlyVisiting = bandsToVisit.pop()
+	bandsVisited.append(bandCurrentlyVisiting)
 
-    website = urllib2.urlopen(bandCurrentlyVisiting).read()
-    
-    soup = BeautifulSoup(website)
-    
-    # Finds band name; needs to extract link.
-    s = soup.find_all(attrs={"class": "band_name"})
-    print 'Visiting [' + str(s[0].next_element.next_element) + ']...'
+	website = urllib2.urlopen(bandCurrentlyVisiting).read()
 
-    bandLinks = soup.find_all(attrs={"class": "lineupBandsRow"})
-    print '[' + str(len(bandLinks)) + '] persons in lineup found.'
-    print(bandLinks[0].next_element.next_element)
+	soup = BeautifulSoup(website)
+
+	# Finds band name; needs to extract link.
+	s = soup.find_all(attrs={"class": "band_name"})
+	print 'Visiting [' + str(s[0].next_element.next_element) + ']...'
+
+	# Takes all bands which belong to a person. 
+	bandLinks = soup.find_all(attrs={"class": "lineupBandsRow"})
+	print '[' + str(len(bandLinks)) + '] persons in lineup found.'
+
+	for bandLink in bandLinks:
+		if "ex-" in str(bandlink.a.previous_sibling):
+			firstBandIsEx = str(bandlink.a.previous_sibling).contains
+			print '>>>' + str(bandLink) + '<<<'
+
+#    print '*** Next Sibling ***'
+#    print(bandLinks[0].a.previous_sibling)
+#    print '*** Next Sibling ***'
 
 #    print(s.next_element)
 
@@ -40,7 +49,7 @@ while (searchDepth > 0):
 
 
 #    print lxml.html.tostring(html)
-    searchDepth-=1
+	searchDepth-=1
 
 print 'Visited [' + str(len(bandsVisited)) + '] bands.'
 
