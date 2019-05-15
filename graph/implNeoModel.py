326d88458b8755da86815f4af786f1da57541db5
Martin Woelke <fnordoglargh@gmail.com>
2019-05-15 23:20:56 +0200

    Started moving stuff around.

---
+++ graph/implNeoModel.py
@@ -0,0 +1,47 @@
+from neomodel import StructuredNode, StringProperty, IntegerProperty, ArrayProperty, DateProperty, RelationshipTo, RelationshipFrom, StructuredRel, config
+from datetime import date
+from graph.choices import *
+
+
+class MemberRelationship(StructuredRel):
+    # start_date = DateProperty()
+    # end_date = DateProperty()
+    pseudonym = StringProperty()
+    instrument = StringProperty()
+
+
+class Band(StructuredNode):
+    emid = IntegerProperty(unique_index=True)
+    name = StringProperty()
+    country = StringProperty(max_length=2, choices=COUNTRIES)
+    locations = ArrayProperty()
+    status = StringProperty(max_length=1, choices=STATUS)
+    formed = DateProperty()
+
+    themes = ArrayProperty()  # Should a theme be a node?
+    current_lineup = RelationshipFrom("Member", "PLAYED_IN", model=MemberRelationship)
+    releases = RelationshipTo('Album', 'RECORDED')
+
+
+class Label(StructuredNode):
+    emid = IntegerProperty(unique_index=True)
+    name = StringProperty()
+    status = StringProperty(max_length=1, choices=LABEL_STATUS)
+    releases = RelationshipFrom('Album', 'RELEASED_ON')
+
+
+class Album(StructuredNode):
+    emid = IntegerProperty(unique_index=True)
+    name = StringProperty()
+    type = StringProperty(max_lenght=1, choices=ALBUM_TYPES)
+    rating = IntegerProperty()
+    release_date = DateProperty()
+    released_on = RelationshipTo('Label', 'RELEASED_ON')
+    recorded_by = RelationshipFrom('Band', 'RECORDED')
+    # Don't forget to crawl for the label!
+
+
+class Member(StructuredNode):
+    emid = IntegerProperty(unique_index=True)
+    name = StringProperty()
+    played_in = RelationshipTo("Band", "PLAYED_IN", model=MemberRelationship)
