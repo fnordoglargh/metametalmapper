9027fa313a0f0ad09d4f2b8de27c3917add5c49e
Martin Woelke <fnordoglargh@gmail.com>
2019-07-21 19:26:32 +0200

    Added more test cases.

--- genre.py
+++ genre.py
@@ -5,6 +5,10 @@ genres_file_path = Path('genres.json')
 
 
 def load_genres():
+    """Loads the JSON genre cache from disk or returns an empty dict if there is no cache yet.
+
+    :return: A dictionary with keys of all known genre strings and as value a list of all genres.
+    """
     if not genres_file_path.exists():
         return dict()
     else:
@@ -12,11 +16,48 @@ def load_genres():
         return json.loads(genre_string)
 
 
+# Bootstrap: Call your own loading function.
 GENRE_CACHE = load_genres()
+# Known strings we want cut from the end of genre.
 GENRE_BLACKLIST = ['(early)', '(mid)', '(later)', 'Metal']
+# Hardcoded dict of all known genres considered on the MA page https://www.metal-archives.com/browse/genre.
+# The key is used up to look up the fitting genre string as on above page.
+GENRE_CORE = {
+    'Black': 'Black',
+    'Death': 'Death',
+    'Doom': 'Doom/Stoner/Sludge',
+    'Stoner': 'Doom/Stoner/Sludge',
+    'Sludge': 'Doom/Stoner/Sludge',
+    'Electronic': 'Electronic/Industrial',
+    'Industrial': 'Electronic/Industrial',
+    'Experimental': 'Experimental/Avant-garde',
+    'Avant-garde': 'Experimental/Avant-garde',
+    'Folk': 'Folk/Viking/Pagan',
+    'Viking': 'Folk/Viking/Pagan',
+    'Pagan': 'Folk/Viking/Pagan',
+    'Gothic': 'Gothic',
+    'Grindcore': 'Grindcore',
+    'Groove': 'Groove',
+    'Heavy': 'Heavy',
+    'Metalcore': 'Metalcore/Deathcore',
+    'Deathcore': 'Metalcore/Deathcore',
+    'Power': 'Power',
+    'Progressive': 'Progressive',
+    'Speed': 'Speed',
+    'Symphonic': 'Symphonic',
+    'Thrash': 'Thrash'
+}
 
 
 def split_genres(genre_string):
+    """Takes any genre string of an band entry on MA and separates the genres into a list. We always cut trailing
+        strings defined in GENRE_BLACKLIST.
+
+    :param genre_string: A string with genres. Could be something really simple like 'Black Metal' or more complex like
+        'Experimental Black Metal/Dark Ambient'. From the latter we extract: 'Experimental Black', 'Black',
+        'Experimental/Avant-garde', 'Dark Ambient'. See the test for more examples.
+    :return: A list of all genres detected in the given genre_string.
+    """
     if genre_string in GENRE_CACHE:
         return GENRE_CACHE[genre_string]
 
@@ -28,10 +69,17 @@ def split_genres(genre_string):
         for inner_genre in split_genres_2nd:
             temp = inner_genre
             for blacked in GENRE_BLACKLIST:
-                if inner_genre.find(blacked) > 0:
-                    temp = temp[:temp.find(blacked)].rstrip()
+                position = temp.find(blacked)
+                if position > 0:
+                    if position + len(blacked) != len(temp):
+                        print()
+                    else:
+                        temp = temp[:position].rstrip()
             if temp not in genre_list:
                 genre_list.append(temp)
+                for key, value in GENRE_CORE.items():
+                    if key in temp and value not in genre_list:
+                        genre_list.append(value)
 
     GENRE_CACHE[genre_string] = genre_list
     return GENRE_CACHE[genre_string]
--- tests/TestGenreCutting.py
+++ tests/TestGenreCutting.py
@@ -1,5 +1,4 @@
 import unittest
-from metalCrawler import *
 import genre
 
 class TestGenreCutting(unittest.TestCase):
@@ -10,7 +9,7 @@ class TestGenreCutting(unittest.TestCase):
         self.assertEqual(expected_cut, result)
 
         result = genre.split_genres('Pagan Black Metal (early), Death Metal (later)')
-        expected_cut = ['Pagan Black', 'Death']
+        expected_cut = ['Pagan Black', 'Black', 'Folk/Viking/Pagan', 'Death']
         self.assertEqual(expected_cut, result)
 
         result = genre.split_genres('Death Metal (early), Black Metal (mid), Black/Heavy/Speed Metal (later)')
@@ -18,15 +17,27 @@ class TestGenreCutting(unittest.TestCase):
         self.assertEqual(expected_cut, result)
 
         result = genre.split_genres('Symphonic Black/Death Metal (early), Melodic Death Metal (later)')
-        expected_cut = ['Symphonic Black', 'Death', 'Melodic Death']
+        expected_cut = ['Symphonic Black', 'Black', 'Symphonic', 'Death', 'Melodic Death']
         self.assertEqual(expected_cut, result)
 
         result = genre.split_genres('Experimental Black Metal/Dark Ambient')
-        expected_cut = ['Experimental Black', 'Dark Ambient']
+        expected_cut = ['Experimental Black', 'Black', 'Experimental/Avant-garde', 'Dark Ambient']
         self.assertEqual(expected_cut, result)
 
         result = genre.split_genres('Melodic Power/Speed Metal/Rock')
-        expected_cut = ['Melodic Power', 'Speed', 'Rock']
+        expected_cut = ['Melodic Power', 'Power', 'Speed', 'Rock']
+        self.assertEqual(expected_cut, result)
+
+        result = genre.split_genres('Industrial Metal')
+        expected_cut = ['Industrial', 'Electronic/Industrial']
+        self.assertEqual(expected_cut, result)
+
+        result = genre.split_genres('Electronic Progressive Death/Thrash Metal')
+        expected_cut = ['Electronic Progressive Death', 'Death', 'Electronic/Industrial', 'Progressive', 'Thrash']
+        self.assertEqual(expected_cut, result)
+
+        result = genre.split_genres('Melodic Death/Doom Metal (early), Industrial/Gothic Metal (mid), Melodic Metalcore (later)')
+        expected_cut = ['Melodic Death', 'Death', 'Doom', 'Doom/Stoner/Sludge', 'Industrial', 'Electronic/Industrial', 'Gothic', 'Melodic Metalcore', 'Metalcore/Deathcore']
         self.assertEqual(expected_cut, result)
 
 
