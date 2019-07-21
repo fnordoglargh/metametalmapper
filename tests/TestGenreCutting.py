033d630923a340f39dd923164ac4f34a8f58c485
Martin Woelke <fnordoglargh@gmail.com>
2019-07-21 19:44:57 +0200

    Refactored test cases to use a collection of test data.

--- tests/TestGenreCutting.py
+++ tests/TestGenreCutting.py
@@ -4,42 +4,28 @@ import genre
 class TestGenreCutting(unittest.TestCase):
 
     def test_cutting(self):
-        result = genre.split_genres('Black Metal')
-        expected_cut = ['Black']
-        self.assertEqual(expected_cut, result)
+        test_strings = [
+            ('Black Metal',
+             ['Black']),
+            ('Pagan Black Metal (early), Death Metal (later)',
+             ['Pagan Black', 'Black', 'Folk/Viking/Pagan', 'Death']),
+            ('Death Metal (early), Black Metal (mid), Black/Heavy/Speed Metal (later)',
+             ['Death', 'Black', 'Heavy', 'Speed']),
+            ('Symphonic Black/Death Metal (early), Melodic Death Metal (later)',
+             ['Symphonic Black', 'Black', 'Symphonic', 'Death', 'Melodic Death']),
+            ('Experimental Black Metal/Dark Ambient',
+             ['Experimental Black', 'Black', 'Experimental/Avant-garde', 'Dark Ambient']),
+            ('Melodic Power/Speed Metal/Rock',
+             ['Melodic Power', 'Power', 'Speed', 'Rock']),
+            ('Industrial Metal',
+             ['Industrial', 'Electronic/Industrial']),
+            ('Electronic Progressive Death/Thrash Metal',
+             ['Electronic Progressive Death', 'Death', 'Electronic/Industrial', 'Progressive', 'Thrash']),
+            ('Melodic Death/Doom Metal (early), Industrial/Gothic Metal (mid), Melodic Metalcore (later)',
+             ['Melodic Death', 'Death', 'Doom', 'Doom/Stoner/Sludge', 'Industrial', 'Electronic/Industrial', 'Gothic', 'Melodic Metalcore', 'Metalcore/Deathcore'])
 
-        result = genre.split_genres('Pagan Black Metal (early), Death Metal (later)')
-        expected_cut = ['Pagan Black', 'Black', 'Folk/Viking/Pagan', 'Death']
-        self.assertEqual(expected_cut, result)
+        ]
 
-        result = genre.split_genres('Death Metal (early), Black Metal (mid), Black/Heavy/Speed Metal (later)')
-        expected_cut = ['Death', 'Black', 'Heavy', 'Speed']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Symphonic Black/Death Metal (early), Melodic Death Metal (later)')
-        expected_cut = ['Symphonic Black', 'Black', 'Symphonic', 'Death', 'Melodic Death']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Experimental Black Metal/Dark Ambient')
-        expected_cut = ['Experimental Black', 'Black', 'Experimental/Avant-garde', 'Dark Ambient']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Melodic Power/Speed Metal/Rock')
-        expected_cut = ['Melodic Power', 'Power', 'Speed', 'Rock']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Industrial Metal')
-        expected_cut = ['Industrial', 'Electronic/Industrial']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Electronic Progressive Death/Thrash Metal')
-        expected_cut = ['Electronic Progressive Death', 'Death', 'Electronic/Industrial', 'Progressive', 'Thrash']
-        self.assertEqual(expected_cut, result)
-
-        result = genre.split_genres('Melodic Death/Doom Metal (early), Industrial/Gothic Metal (mid), Melodic Metalcore (later)')
-        expected_cut = ['Melodic Death', 'Death', 'Doom', 'Doom/Stoner/Sludge', 'Industrial', 'Electronic/Industrial', 'Gothic', 'Melodic Metalcore', 'Metalcore/Deathcore']
-        self.assertEqual(expected_cut, result)
-
-
-
-
+        for test in test_strings:
+            result = genre.split_genres(test[0])
+            self.assertEqual(result, test[1])
