3aa1e9bc89000ff7a0b9bf2132575fe05c8303ab
Martin Woelke <fnordoglargh@gmail.com>
2019-07-22 14:03:38 +0200

    Commented the rest in genres.

--- genre.py
+++ genre.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import json
 
-genres_file_path = Path('genres.json')
+# Hardcoded file path for the genre cache.
+GENRES_FILE_PATH = Path('genre_cache.json')
 
 
 def load_genres():
@@ -9,17 +10,19 @@ def load_genres():
 
     :return: A dictionary with keys of all known genre strings and as value a list of all genres.
     """
-    if not genres_file_path.exists():
+    if not GENRES_FILE_PATH.exists():
         return dict()
     else:
-        genre_string = genres_file_path.read_text(encoding="utf-8")
+        genre_string = GENRES_FILE_PATH.read_text(encoding="utf-8")
         return json.loads(genre_string)
 
 
 # Bootstrap: Call your own loading function.
 GENRE_CACHE = load_genres()
+
 # Known strings we want cut from the end of genre.
 GENRE_BLACKLIST = ['(early)', '(mid)', '(later)', 'Metal']
+
 # Hardcoded dict of all known genres considered on the MA page https://www.metal-archives.com/browse/genre.
 # The key is used up to look up the fitting genre string as on above page.
 GENRE_CORE = {
@@ -53,40 +56,48 @@ def split_genres(genre_string):
     """Takes any genre string of an band entry on MA and separates the genres into a list. We always cut trailing
         strings defined in GENRE_BLACKLIST.
 
+        This function is written in way that always considers the core genres of MA. A composite genre like
+        'Experimental Black Metal' will be split into a list with the following genres: 'Experimental Black', 'Black'
+        and 'Experimental/Avant-garde'. The first in the list is the raw extracted string. The other two are the core
+        genres we deduct from the first part.
+
     :param genre_string: A string with genres. Could be something really simple like 'Black Metal' or more complex like
         'Experimental Black Metal/Dark Ambient'. From the latter we extract: 'Experimental Black', 'Black',
         'Experimental/Avant-garde', 'Dark Ambient'. See the test for more examples.
     :return: A list of all genres detected in the given genre_string.
     """
+    # Early return if the string is cached.
     if genre_string in GENRE_CACHE:
         return GENRE_CACHE[genre_string]
 
     genre_list = []
-    split_genres_1st = genre_string.split(', ')
 
-    for outer_genre in split_genres_1st:
-        split_genres_2nd = outer_genre.split('/')
-        for inner_genre in split_genres_2nd:
-            temp = inner_genre
+    # Iterate over the split genre_string.
+    for outer_genre in genre_string.split(', '):
+        # Each outer_genre may consist of multiple genres (separated by a '/'). So we split those too.
+        for inner_genre in outer_genre.split('/'):
+            temp_genre = inner_genre
+            # Remove the blacklisted strings at the end.
             for blacked in GENRE_BLACKLIST:
-                position = temp.find(blacked)
-                if position > 0:
-                    if position + len(blacked) != len(temp):
-                        print()
-                    else:
-                        temp = temp[:position].rstrip()
-            if temp not in genre_list:
-                genre_list.append(temp)
+                position = temp_genre.find(blacked)
+                #  The second condition is necessary to detect genres with a leading 'Metal' in them.
+                if position > 0 and position + len(blacked) == len(temp_genre):
+                    temp_genre = temp_genre[:position].rstrip()
+            if temp_genre not in genre_list:
+                genre_list.append(temp_genre)
                 for key, value in GENRE_CORE.items():
-                    if key in temp and value not in genre_list:
+                    # Add the value from the core genre dict if it is not already there.
+                    if key in temp_genre and value not in genre_list:
                         genre_list.append(value)
 
     GENRE_CACHE[genre_string] = genre_list
-    return GENRE_CACHE[genre_string]
+    return genre_list
 
 
 def save_genres():
-    actual_genre_file = open(genres_file_path, "w", encoding="utf-8")
+    """Dumps the cache into a JSON file.
+    """
+    actual_genre_file = open(GENRES_FILE_PATH, "w", encoding="utf-8")
     json_database_string = json.dumps(GENRE_CACHE)
     actual_genre_file.write(json_database_string)
     actual_genre_file.close()
