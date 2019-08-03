5d0ad817daebbf79a18fbd0681e40ca19140f39d
Martin Woelke <fnordoglargh@gmail.com>
2019-08-03 15:31:51 +0200

    Implemented basic GraphML exporter.

--- graph/exportGraph.py
+++ graph/exportGraph.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
+import logging
+from global_helpers import FOLDER_EXPORTS
+from datetime import datetime
 
 
 class GraphExportContext:
@@ -8,7 +11,7 @@ class GraphExportContext:
         self._strategy = strategy
 
     def export_graph(self, data_dict):
-        self._strategy.export_bands_interface(data_dict)
+        self._strategy.export_graph_interface(data_dict)
 
 
 class GraphExportStrategy(metaclass=ABCMeta):
@@ -19,5 +22,48 @@ class GraphExportStrategy(metaclass=ABCMeta):
 
 
 class GraphMLExporter(GraphExportStrategy):
-    def export_graph_interface(self, data_dict):
-        pass
+
+    def __init__(self):
+        self.logger = logging.getLogger('GraphMLExporter')
+
+    def export_graph_interface(self, data_dict: dict):
+        self.logger.info('Starting GraphML export to file.')
+
+        header = (
+            f'<?xml version="1.0" encoding="UTF-8"?>\n'
+            f'<graphml xmlns="http://graphml.graphdrawing.org/xmlns"\n'
+            f'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n'
+            f'xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n'
+            f'<key id="d0" for="node" attr.name="band_name" attr.type="string"/>\n'
+            f'<graph id="G" edgedefault="undirected">\n'
+        )
+
+        footer = (
+            f'</graph>\n'
+            f'</graphml>'
+        )
+
+        actual_time = datetime.now()
+        time_stamp = f'{actual_time.date()}_{actual_time.time().strftime("%H%M%S")}'
+        db_path = Path(f"{FOLDER_EXPORTS}/bands_{time_stamp}.graphml")
+        export_file = open(db_path, "w", encoding="utf-8")
+        export_file.write(header)
+
+        # Go through collection once to create nodes.
+        for node, payload in data_dict.items():
+            band_name = payload["name"]
+
+            if '&' in band_name:
+                band_name = band_name.replace('&', '&amp;')
+
+            export_file.write(f'<node id="n{node}"><data key="d0">{band_name}</data></node>\n')
+
+        # The second time we write the connections. This might seem odd but Cytoscape does not like the connections
+        # between the nodes.
+        counter = 0
+        for node, payload in data_dict.items():
+            for relation in payload['relations']:
+                export_file.write(f'<edge id="e{counter}" source="n{node}" target="n{relation}"/>\n')
+                counter += 1
+        export_file.write(footer)
+        export_file.close()
