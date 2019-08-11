49ef84b451e821d5e9927cda2542e295a0c89875
Martin Woelke <fnordoglargh@gmail.com>
2019-08-11 20:43:05 +0200

    Commented key gettin function.

--- global_helpers.py
+++ global_helpers.py
@@ -10,4 +10,10 @@ FOLDERS_MAIN = [FOLDER_LINKS, FOLDER_DB, FOLDER_EXPORTS]
 
 
 def get_dict_key(source_dict, value):
+    """Gets the key for a given value from a dict.
+
+    :param source_dict: The dict for which we want the key.
+    :param value: The value which is searched for in the dict.
+    :return: The key for the provided value.
+    """
     return list(source_dict.keys())[list(source_dict.values()).index(value)]
