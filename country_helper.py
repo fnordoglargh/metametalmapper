eadc7e0aa06fa737a2b5e4f3e0c9dc7102028386
Martin Woelke <fnordoglargh@gmail.com>
2019-11-21 23:27:28 +0100

    Finished europe's regions in country helper.

--- country_helper.py
+++ country_helper.py
@@ -2,13 +2,22 @@ from pathlib import Path
 
 REG_NC = ("NCO", "Nordic Countries", ["DK", "SE", "NO", "IS", "FI", "GL", "FO", "AX", "SJ"])
 REG_SCA = ("SCA", "Scandinavia",  ["DK", "SE", "NO", "IS", "FI", "FO", "AX"])
-REG_EUR_WEST = ("WEU", "Western Europe", ["BE", "FR", "IE", "LU", "MC", "NL", "GB"])
-REG_EUR_CENTRAL = ("CEU", "Central Europe", ["AT", "CZ", "DE", "HU", "LI", "PL", "SK", "CH"])
+
+
+# Europe's region definitions: https://en.wikipedia.org/wiki/Central_Europe#/media/File:Grossgliederung_Europas-en.svg
+REG_EUR_NORTH = ("NEU", "Northern Europe", ["DK", "SE", "NO", "IS", "FI"])
+REG_EUR_WEST = ("WEU", "Western Europe", ["BE", "FR", "IE", "MC", "NL", "GB", "MC", "AD"])
+REG_EUR_CENTRAL = ("CEU", "Central Europe", ["AT", "CZ", "DE", "HU", "LI", "PL", "SK", "CH", "LU", "EE", "LV", "LT", "HR", "SI"])
+REG_EUR_SOUTH = ("SEU", "Southern Europe", ["PT", "ES", "IT", "SM", "MT"])
+REG_EUR_EAST = ("EEU", "Eastern Europe", ["AU", "BY", "RU"])
+# As of Nov 2019: No Bands in Kosovo.
+REG_EUR_SOUTH_EAST = ("SEE", "Southeastern Europe", ["RO", "MD", "BG", "RS", "BA", "MK", "GR", "ME", "AL", "CY"])
+REG_EURO = ("EURO", "Europe", list(set(REG_EUR_NORTH[2] + REG_EUR_WEST[2] + REG_EUR_CENTRAL[2] + REG_EUR_SOUTH[2] + REG_EUR_EAST[2] + REG_EUR_SOUTH_EAST[2])))
 
 REG_AMC_NORTH = ("NAM", "North America", ["US", "CA", "MX", "PR", "TC", "AG", "BS", "BB", "BZ", "CR", "CU", "DM", "DO", "SV", "GD", "GT", "HT", "HN", "JM", "NI", "PA", "KN", "LC", "VC", "TT"])
 REG_AMC_CENTRAL = ("CAM", "Central America", ["BZ", "CR", "SV", "GT", "HN", "NI", "PA"])
 REG_AMC_SOUTH = ("SAM", "South America", ["AR", "BO", "BR", "CL", "CO", "EC", "GY", "PY", "PE", "SR", "UY", "VE"])
-REG_AMC = ("AMC", "Americas", list(set(REG_AMC_CENTRAL[2] + REG_AMC_NORTH[2] + REG_AMC_SOUTH[2])))
+REG_AMS = ("AMS", "Americas", list(set(REG_AMC_CENTRAL[2] + REG_AMC_NORTH[2] + REG_AMC_SOUTH[2])))
 
 # As of Nov 2019: No bands in TL (Timor Leste).
 REG_ASE = ("ASE", "South East Asia", ["BN", "KH", "ID", "LA", "MY", "MM", "PH", "SG", "TH", "VN"])
@@ -26,12 +35,16 @@ REG_TST = ("TST", "Test", ["IS", "GL", "FO", "AX", "SJ"])
 REGIONS = {
     REG_NC[0]: REG_NC,
     REG_SCA[0]: REG_SCA,
+    REG_EUR_NORTH[0]: REG_EUR_NORTH,
     REG_EUR_WEST[0]: REG_EUR_WEST,
     REG_EUR_CENTRAL[0]: REG_EUR_CENTRAL,
+    REG_EUR_SOUTH[0]: REG_EUR_SOUTH,
+    REG_EUR_SOUTH_EAST[0]: REG_EUR_SOUTH_EAST,
+    REG_EURO[0]: REG_EURO,
     REG_AMC_NORTH[0]: REG_AMC_NORTH,
     REG_AMC_CENTRAL[0]: REG_AMC_CENTRAL,
     REG_AMC_SOUTH[0]: REG_AMC_SOUTH,
-    REG_AMC[0]: REG_AMC,
+    REG_AMS[0]: REG_AMS,
     REG_ASE[0]: REG_ASE,
     REG_ASA[0]: REG_ASA,
     REG_ACA[0]: REG_ACA,
