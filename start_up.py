174eb4e2fd714fcf1f584cd7b214e37cce4d322b
Martin Woelke <fnordoglargh@gmail.com>
2020-08-09 02:37:34 +0200

    Removed all long parameters.

--- start_up.py
+++ start_up.py
@@ -5,16 +5,22 @@ from country_helper import COUNTRY_NAMES, REGIONS, print_regions, print_countrie
 from metal_crawler import crawl_countries
 
 file_name_a = BAND_LINK_FILE_NAME.format('NN')
+single_text = 'Crawls the given short link (e.g. Darkthrone/146) and all connected bands.'
 all_links_text = f'Crawls all countries for bands and saves them in files named {file_name_a} (where NN is the two ' \
     f'letter short form of a given country). The files are put into sub-folder "{FOLDER_LINKS}". This action can take '\
     'almost 10 minutes.'
 all_countries_text = 'Crawls the supplied countries (e.g. NO for Norway) and uses the standard file name together ' \
-    'with the ID to write a file with all band links from the given country. See list (-l) below. The country shorts ' \
-    'must always be separated by commas without spaces or be enclosed by quotation-marks if they contain spaces.'
-region_text = '-r <region ID>: Crawls a predefined region (call -l for example IDs or try NCO to get short links of ' \
+    'with the ID to write a file with all band links from the given country. See list (-l) below.'
+region_text = '-r <region ID>: Crawls a predefined region (call -l for sample IDs or try NCO to get short links of ' \
     'all Nordic Countries).'
 crawl_text = 'Crawls a file with short links either from running -a, -i, -c or your own. If a region or country short '\
     f'(see -l) is specified, it will try finding a generated file in sub-folder "{FOLDER_LINKS}".'
+analyze_full_text = 'Prints and exports a raw data report of the active database and also exports a GraphML file of ' \
+    'all bands (including their implicit connections through artists). Either use "ALL" for the entire database or a ' \
+    'list of country shorts or regions as parameter. Note: The country analysis depends on having all bands for a ' \
+    'given country. It will function properly but calculate bogus numbers.'
+analyze_light_text = 'The "diet version" of -y without the country report. Use this mode to analyze data from the ' \
+    'crawl mode -s.'
 
 list_text = 'List available countries and regions.'
 
@@ -35,39 +41,41 @@ arg_parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelp
 
         NOTE: Parameters are given priority based on their stage and cannot be mixed.
     '''))
-arg_parser.add_argument('-s', '--single',
-                        help='Crawls the given short link (e.g. Darkthrone/146) and all connected bands.')
-arg_parser.add_argument('-a', '--all_links', action="store_true", help=all_links_text)
-arg_parser.add_argument('-i', '--iso_countries', nargs='+', help=all_countries_text)
-arg_parser.add_argument('-r', '--region', help=region_text)
-arg_parser.add_argument('-c', '--crawl', nargs='?', const=not_set, help=crawl_text)
-arg_parser.add_argument('-l', '--list', action="store_true", help=list_text)
+arg_parser.add_argument('-s', help=single_text, metavar='MA_SHORT_LINK')
+arg_parser.add_argument('-a', action="store_true", help=all_links_text)
+arg_parser.add_argument('-i', nargs='+', help=all_countries_text, metavar='COUNTRY_SHORT')
+arg_parser.add_argument('-r', help=region_text, metavar='REGION_ID')
+arg_parser.add_argument('-c', help=crawl_text, metavar='FILE_OR_REGION_OR_COUNTRY_SHORT')
+arg_parser.add_argument('-y', nargs='+', help=analyze_full_text, metavar='REGION_OR_COUNTRY_SHORT')
+arg_parser.add_argument('-z', action="store_true", help=analyze_light_text)
+arg_parser.add_argument('-l', action="store_true", help=list_text)
 args = arg_parser.parse_args()
 
-# Test the arguments
-if args.all_links:
+# All countries
+if args.a:
     print('-a is set with no params.')
 
-# Check if single mode can be used.
-if args.single is not None:
-    print(f'-s {args.single}')
+# Single mode
+if args.s is not None:
+    print(f'-s {args.s}')
 
-# Check if -c --iso_countries can be used.
-if args.iso_countries is not None and len(args.iso_countries) > 0:
-    for country in args.iso_countries:
+# ISO countries
+if args.i is not None and len(args.i) > 0:
+    for country in args.i:
         country = country.upper()
         if country not in COUNTRY_NAMES.keys():
             print(f'{country} is not a valid ISO country short.')
         else:
-            print(f'-c: {args.iso_countries}')
+            print(f'-c: {country}')
 
-if args.region is not None:
-    if args.region in REGIONS:
-        print(args.region)
+# Region
+if args.r is not None:
+    if args.r in REGIONS:
+        print(args.r)
     else:
-        print(f'Not a valid region: {args.region}')
+        print(f'Not a valid region: {args.r}')
 
-if args.list:
+if args.l:
     country_string = print_countries(4, crawl_countries())
     print()
     print('Available countries:')
@@ -77,11 +85,11 @@ if args.list:
     print('Available regions:')
     print(regions)
 
-if args.crawl is not None:
-    if args.crawl is not not_set:
+if args.c is not None:
+    if args.c is not not_set:
         # Test if parameter is a valid region or country.
-        if args.crawl in COUNTRY_NAMES.keys() or args.crawl in REGIONS.keys():
-            country_region_file = Path(f'{FOLDER_LINKS}/{args.crawl}{LINK_EXTENSION}')
+        if args.c in COUNTRY_NAMES.keys() or args.c in REGIONS.keys():
+            country_region_file = Path(f'{FOLDER_LINKS}/{args.c}{LINK_EXTENSION}')
 
             if country_region_file.is_file():
                 print('Valid region/country file.')
@@ -90,7 +98,7 @@ if args.crawl is not None:
 
             print(f'  {country_region_file}')
         else:
-            link_file = Path(args.crawl)
+            link_file = Path(args.c)
 
             if link_file.is_file():
                 print(f'Seems to be a link file: {link_file}')
@@ -98,3 +106,19 @@ if args.crawl is not None:
                 print('Not a valid file, country or region short.')
     else:
         print('Parameter missing.')
+
+if args.y is not None:
+    if len(args.y) is 1 and args.y[0].upper() == 'ALL':
+        print('Analyse entire DB.')
+    else:
+        for country in args.y:
+            if country in COUNTRY_NAMES.keys() or country in REGIONS.keys():
+                print(f'Analyze {country}.')
+            else:
+                print(f'Unknown country/region: {country}')
+
+
+if args.z:
+    print('Small Analysis.')
+
+# arg_parser.print_help()
