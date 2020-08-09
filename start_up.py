e13aab60edc99963a0a44795a77c2d657a815c35
Martin Woelke <fnordoglargh@gmail.com>
2020-08-09 18:09:57 +0200

    Added a main function.

--- start_up.py
+++ start_up.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+
+import sys
 import argparse
 import textwrap
+
 from global_helpers import *
 from country_helper import COUNTRY_NAMES, REGIONS, print_regions, print_countries
 from metal_crawler import crawl_countries
@@ -26,99 +30,102 @@ list_text = 'List available countries and regions.'
 
 # Indication of a parameter
 not_set = 'not_set'
-default_text = 'lazy_default'
-choices = COUNTRY_NAMES.keys()
 
-arg_parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=textwrap.dedent('''\
-        Crawls and (meta) analyzes date from M-A.
 
-        It's run in two to three stages:
-            1.  Get a list of bands (-a, -c, -r).
-            2a. Crawl one band through its short link (-s <link>) without stage 1.
-            2b. Crawl a list of bands generated in stage 1 (by filename).
-            3.  Analyze the gathered data  
+def main():
+    arg_parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                         description=textwrap.dedent('''\
+            Crawls and (meta) analyzes date from M-A.
+    
+            It's run in two to three stages:
+                1.  Get a list of bands (-a, -c, -r).
+                2a. Crawl one band through its short link (-s <link>) without stage 1.
+                2b. Crawl a list of bands generated in stage 1 (by filename).
+                3.  Analyze the gathered data  
+    
+            NOTE: Parameters are given priority based on their stage and cannot be mixed.
+        '''))
+    arg_parser.add_argument('-s', help=single_text, metavar='MA_SHORT_LINK')
+    arg_parser.add_argument('-a', action="store_true", help=all_links_text)
+    arg_parser.add_argument('-i', nargs='+', help=all_countries_text, metavar='COUNTRY_SHORT')
+    arg_parser.add_argument('-r', help=region_text, metavar='REGION_ID')
+    arg_parser.add_argument('-c', help=crawl_text, metavar='FILE_OR_REGION_OR_COUNTRY_SHORT')
+    arg_parser.add_argument('-y', nargs='+', help=analyze_full_text, metavar='REGION_OR_COUNTRY_SHORT')
+    arg_parser.add_argument('-z', action="store_true", help=analyze_light_text)
+    arg_parser.add_argument('-l', action="store_true", help=list_text)
+    args = arg_parser.parse_args()
 
-        NOTE: Parameters are given priority based on their stage and cannot be mixed.
-    '''))
-arg_parser.add_argument('-s', help=single_text, metavar='MA_SHORT_LINK')
-arg_parser.add_argument('-a', action="store_true", help=all_links_text)
-arg_parser.add_argument('-i', nargs='+', help=all_countries_text, metavar='COUNTRY_SHORT')
-arg_parser.add_argument('-r', help=region_text, metavar='REGION_ID')
-arg_parser.add_argument('-c', help=crawl_text, metavar='FILE_OR_REGION_OR_COUNTRY_SHORT')
-arg_parser.add_argument('-y', nargs='+', help=analyze_full_text, metavar='REGION_OR_COUNTRY_SHORT')
-arg_parser.add_argument('-z', action="store_true", help=analyze_light_text)
-arg_parser.add_argument('-l', action="store_true", help=list_text)
-args = arg_parser.parse_args()
+    # All countries
+    if args.a:
+        print('-a is set with no params.')
 
-# All countries
-if args.a:
-    print('-a is set with no params.')
+    # Single mode
+    if args.s is not None:
+        print(f'-s {args.s}')
 
-# Single mode
-if args.s is not None:
-    print(f'-s {args.s}')
+    # ISO countries
+    if args.i is not None and len(args.i) > 0:
+        for country in args.i:
+            country = country.upper()
+            if country not in COUNTRY_NAMES.keys():
+                print(f'{country} is not a valid ISO country short.')
+            else:
+                print(f'-c: {country}')
 
-# ISO countries
-if args.i is not None and len(args.i) > 0:
-    for country in args.i:
-        country = country.upper()
-        if country not in COUNTRY_NAMES.keys():
-            print(f'{country} is not a valid ISO country short.')
+    # Region
+    if args.r is not None:
+        if args.r in REGIONS:
+            print(args.r)
         else:
-            print(f'-c: {country}')
+            print(f'Not a valid region: {args.r}')
 
-# Region
-if args.r is not None:
-    if args.r in REGIONS:
-        print(args.r)
-    else:
-        print(f'Not a valid region: {args.r}')
+    if args.l:
+        country_string = print_countries(4, crawl_countries())
+        print()
+        print('Available countries:')
+        print(country_string)
+        regions = print_regions()
+        print()
+        print('Available regions:')
+        print(regions)
 
-if args.l:
-    country_string = print_countries(4, crawl_countries())
-    print()
-    print('Available countries:')
-    print(country_string)
-    regions = print_regions()
-    print()
-    print('Available regions:')
-    print(regions)
+    if args.c is not None:
+        if args.c is not not_set:
+            # Test if parameter is a valid region or country.
+            if args.c in COUNTRY_NAMES.keys() or args.c in REGIONS.keys():
+                country_region_file = Path(f'{FOLDER_LINKS}/{args.c}{LINK_EXTENSION}')
 
-if args.c is not None:
-    if args.c is not not_set:
-        # Test if parameter is a valid region or country.
-        if args.c in COUNTRY_NAMES.keys() or args.c in REGIONS.keys():
-            country_region_file = Path(f'{FOLDER_LINKS}/{args.c}{LINK_EXTENSION}')
+                if country_region_file.is_file():
+                    print('Valid region/country file.')
+                else:
+                    print('File not found.')
 
-            if country_region_file.is_file():
-                print('Valid region/country file.')
+                print(f'  {country_region_file}')
             else:
-                print('File not found.')
+                link_file = Path(args.c)
 
-            print(f'  {country_region_file}')
+                if link_file.is_file():
+                    print(f'Seems to be a link file: {link_file}')
+                else:
+                    print('Not a valid file, country or region short.')
         else:
-            link_file = Path(args.c)
+            print('Parameter missing.')
 
-            if link_file.is_file():
-                print(f'Seems to be a link file: {link_file}')
-            else:
-                print('Not a valid file, country or region short.')
-    else:
-        print('Parameter missing.')
+    if args.y is not None:
+        if len(args.y) is 1 and args.y[0].upper() == 'ALL':
+            print('Analyse entire DB.')
+        else:
+            for country in args.y:
+                if country in COUNTRY_NAMES.keys() or country in REGIONS.keys():
+                    print(f'Analyze {country}.')
+                else:
+                    print(f'Unknown country/region: {country}')
 
-if args.y is not None:
-    if len(args.y) is 1 and args.y[0].upper() == 'ALL':
-        print('Analyse entire DB.')
-    else:
-        for country in args.y:
-            if country in COUNTRY_NAMES.keys() or country in REGIONS.keys():
-                print(f'Analyze {country}.')
-            else:
-                print(f'Unknown country/region: {country}')
+    if args.z:
+        print('Small Analysis.')
+
+    # arg_parser.print_help()
 
 
-if args.z:
-    print('Small Analysis.')
-
-# arg_parser.print_help()
+if __name__ == '__main__':
+    main()
