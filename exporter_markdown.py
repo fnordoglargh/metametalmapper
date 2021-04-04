78f02846ba0f0f18511524a9bb3071650a7cb051
Martin Woelke <fnordoglargh@gmail.com>
2021-04-04 13:33:19 +0200

    Added details to "world" report.

--- exporter_markdown.py
+++ exporter_markdown.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 
 from exporter_strategy import ExportingStrategy
 from export_data import ExportData
-from country_helper import COUNTRY_TO_REGIONS, COUNTRY_NAMES
+from country_helper import COUNTRY_TO_REGIONS, COUNTRY_NAMES, COUNTRY_POPULATION
+from graph.choices import GENDER
 
 
 class ExporterMarkdown(ExportingStrategy):
@@ -10,18 +11,45 @@ class ExporterMarkdown(ExportingStrategy):
         super().__init__('markdown')
 
     def do_export(self, export_data: ExportData):
-        missing_countries = sorted(COUNTRY_NAMES - COUNTRY_TO_REGIONS.keys())
-        # missing_countries = COUNTRY_NAMES - export_data.country_data.keys()
-
         texts = defaultdict(str)
-        texts['world'] = '# Countries without metal bands\n\n' \
-                         f'No metal bands are known in {len(missing_countries)} (out of {len(COUNTRY_NAMES)}) ' \
-                         f'countries.\n\n'
+        texts['world'] = '# Summary\n\n'
+
+        number_people = 0
+
+        for population in COUNTRY_POPULATION.values():
+            number_people += int(population)
+
+        number_artists = 0
+        gender_totals = {}
+
+        for gender in GENDER.keys():
+            gender_totals[gender] = 0
+
+        for country, gender_info in export_data.genders_country.items():
+            number_artists += gender_info.totals
+
+            for gender, number in gender_info.genders.items():
+                gender_totals[gender] += number
+
+        missing_countries = sorted(COUNTRY_NAMES - COUNTRY_TO_REGIONS.keys())
+
+        texts['world'] += f'Approximately {number_people/1000000000:.3f} billion people lived on earth that year. ' \
+                          f'{export_data.bands_total} bands hail from {len(export_data.country_data)} countries. '
+        texts['world'] += f'No metal bands are known in {len(missing_countries)} (out of {len(COUNTRY_NAMES)}) ' \
+                          'countries.\n\n'
+
+        texts['world'] += '# Gender Overview\n\n' \
+                          f'The {number_artists} people who played in metal bands consist of:\n\n'
+
+        for gender, number in gender_totals.items():
+            texts['world'] += f'* {GENDER[gender]}: {number} ({number/number_artists*100:.2f}%)\n'
+
+        texts['world'] += '# Countries without metal bands\n\n'
 
         for country in missing_countries:
             texts['world'] += f'  * {COUNTRY_NAMES[country]}\n'
         # Write metal world.
         file_name = self.generate_file_name('world', 'md')
         file_name.write_text(texts['world'], encoding='utf-8')
-        self.logger.info(f'    JSON: {file_name}')
+        self.logger.info(f'World report saved under: {file_name}')
 
