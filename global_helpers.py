d06d1d4bb5ac24d5d2a75276018cde7f61e88911
Martin Woelke <fnordoglargh@gmail.com>
2022-08-16 23:15:50 +0200

    Corrected typos.

--- global_helpers.py
+++ global_helpers.py
@@ -28,10 +28,10 @@ def get_export_path(name: str, extension: str):
     """Helper function to create off the shelf Path objects to export data. Data is always exported into the folder
         FOLDER_EXPORTS. The complete path to a file may look like "exports/name_timestamp.extension".
 
-    :param name: Name of the category of date you want to to safe. Empty strings will be overridden with
+    :param name: Name of the category of date you want to safe. Empty strings will be overridden with
         "no_name_given".
     :param extension: Name of the file extension. A dot will be provided if it's missing.
-    :return: A ready made Path object to safe data in.
+    :return: A ready-made Path object to safe data in.
     """
     if len(name) is 0:
         name = 'no_name_given'
